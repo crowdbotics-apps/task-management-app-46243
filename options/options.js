44c1218683a734f52d278e5eb45add912678119f
Crowdbotics <team@crowdbotics.com>
2024-02-11 01:01:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "task-management-app-46243",
+  url: "https://task-management-app-46243.botics.co",
+  api: "https://task-management-app-46243.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
